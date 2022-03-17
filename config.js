293a584d8f8f628f70ae28d36d83afca47a4e9ba
nexusyes946 <76816403+nexusyes946@users.noreply.github.com>
2022-03-17 20:45:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
-  title: 'yun yu yuan',
-  githubName: 'yunyuyuan',
-  githubRepo: 'nuxt2-blog',
+  title: 'nexusyes946',
+  githubName: 'NEXUS',
+  githubRepo: 'NEXUS-blog',
   githubBranch: 'master',
   domain: 'https://blog.halberd.cn',
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原的博客',
+  SEO_description: '精于心，简于形',
+  SEO_keywords: 'NEXUS,NEXUS\'s blog,NEXUS的小站',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
 }
